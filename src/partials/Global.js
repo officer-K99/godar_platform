c0529e1579026442bf3a8b7b5d836b30b6dc0d03
officer-K99 <76909380+officer-K99@users.noreply.github.com>
2022-10-02 11:11:29 +0330

    font change

--- src/partials/Global.js
+++ src/partials/Global.js
@@ -13,21 +13,16 @@ export const GlobalStyles = createGlobalStyle`
   -o-box-sizing: border-box;
   box-sizing: border-box;
   outline: none;
-  font-family: 'IRANSans';
 }
 
 body {
     direction: rtl;
   margin: 0;
-  font-family: 'IRANSans';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: ${({ theme }) => theme.BgColors.body};
 }
 
-code {
-  font-family: IranianSans;
-}
 
 li {
   list-style-type: none;
@@ -37,11 +32,6 @@ a {
   text-decoration: none;
 }
 
-@font-face {
-  font-family: 'IRANSans';
-  src: local('IRANSans'),
-   url("./font/Iranian-Sans.ttf") format("truetype");
-  font-weight: normal;
-  }
+
   
   `;
